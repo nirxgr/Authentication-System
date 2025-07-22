cdac2492e42e02513ecb801eda5fa32811ed3160
Niraj Gurung <grgniraj01@gmail.com>
2025-07-22 22:56:33 +0545

    Final push

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -83,7 +83,7 @@ const ResetPassword = () => {
   }
 
   return (
-    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-sky-100 via-indigo-100 to-purple-100'>
       <img onClick = {() => navigate('/')}src={assets.logo} alt='' className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
 
       {/* enter email id */}
