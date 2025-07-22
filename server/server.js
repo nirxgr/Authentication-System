741f18019577f08e715a7232f929a945b8ad4f11
Niraj Gurung <grgniraj01@gmail.com>
2025-07-22 22:28:13 +0545

    Code fixing

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 4000
 connectDB();
 
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://authentication-system-henna.vercel.app'
+];
 
 app.use(express.json());
 app.use(cookieParser());
